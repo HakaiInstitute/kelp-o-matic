72202a4e6bc6fa400d17b2ae64ec9de057b4cf18
Taylor Denouden <taylordenouden@gmail.com>
2022-08-16 12:07:40 -0700

    Update mussel detection weights

--- hakai_segmentation/data/__init__.py
+++ hakai_segmentation/data/__init__.py
@@ -3,6 +3,6 @@ from pkg_resources import resource_filename
 lraspp_kelp_presence_torchscript_path = resource_filename(
     __name__, 'LRASPP_MobileNetV3_kelp_presence_jit.pt')
 lraspp_mussel_presence_torchscript_path = resource_filename(
-    __name__, 'LRASPP_MobileNetV3_mussel_presence_jit.pt')
+    __name__, 'LRASPP_MobileNetV3_mussel_presence_jit_v2.pt')
 lraspp_kelp_species_torchscript_path = resource_filename(
     __name__, 'LRASPP_MobileNetV3_kelp_species_jit.pt')
