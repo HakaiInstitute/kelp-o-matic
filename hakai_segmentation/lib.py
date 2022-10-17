41fe1f7e12294e9150fd1b543f7988f386390e3c
Taylor Denouden <taylordenouden@gmail.com>
2022-10-17 09:35:39 -0700

    Remove `reload-on-batch` options
    Doesn't fix memory issues, not useful

--- hakai_segmentation/lib.py
+++ hakai_segmentation/lib.py
@@ -4,8 +4,7 @@ from hakai_segmentation.models import KelpPresenceSegmentationModel, KelpSpecies
 
 
 def find_kelp(source: str, dest: str, species: bool = False,
-              crop_size: int = 256, padding: int = 128, batch_size: int = 2, use_gpu: bool = True,
-              reload_model_on_batch: bool = False):
+              crop_size: int = 256, padding: int = 128, batch_size: int = 2, use_gpu: bool = True):
     """Detect kelp in image at path `source` and output the resulting classification raster to file at path `dest`.
 
     :param source: Input image with Byte data type.
@@ -15,20 +14,17 @@ def find_kelp(source: str, dest: str, species: bool = False,
     :param padding: The number of context pixels added to each side of the cropped image squares.
     :param batch_size: The batch size of cropped image sections to process together.
     :param use_gpu: Disable Cuda GPU usage and run on CPU only.
-    :param reload_model_on_batch: Experimental flag to reload the model for each data batch. May resolve memory issues.
     """
     if species:
         model = KelpSpeciesSegmentationModel(use_gpu=use_gpu)
     else:
         model = KelpPresenceSegmentationModel(use_gpu=use_gpu)
     GeotiffSegmentation(model, source, dest,
-                        crop_size=crop_size, padding=padding, batch_size=batch_size,
-                        reload_model_on_batch=reload_model_on_batch)()
+                        crop_size=crop_size, padding=padding, batch_size=batch_size)()
 
 
 def find_mussels(source: str, dest: str,
-                 crop_size: int = 256, padding: int = 128, batch_size: int = 2, use_gpu: bool = True,
-                 reload_model_on_batch: bool = False):
+                 crop_size: int = 256, padding: int = 128, batch_size: int = 2, use_gpu: bool = True):
     """Detect mussels in image at path `source` and output the resulting classification raster to file at path `dest`.
 
     :param source: Input image with Byte data type.
@@ -37,9 +33,7 @@ def find_mussels(source: str, dest: str,
     :param padding: The number of context pixels added to each side of the cropped image squares.
     :param batch_size: The batch size of cropped image sections to process together.
     :param use_gpu: Disable Cuda GPU usage and run on CPU only.
-    :param reload_model_on_batch: Experimental flag to reload the model for each data batch. May resolve memory issues.
     """
     model = MusselPresenceSegmentationModel(use_gpu=use_gpu)
     GeotiffSegmentation(model, source, dest,
-                        crop_size=crop_size, padding=padding, batch_size=batch_size,
-                        reload_model_on_batch=reload_model_on_batch)()
+                        crop_size=crop_size, padding=padding, batch_size=batch_size)()
