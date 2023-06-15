69128b458655b38fc1957011ed36d475cb9b2b9f
Taylor Denouden <taylordenouden@gmail.com>
2023-06-15 16:50:47 -0700

    Update resource file handling

--- kelp_o_matic/data/__init__.py
+++ kelp_o_matic/data/__init__.py
@@ -1,11 +1,18 @@
-from pkg_resources import resource_filename
+import atexit
+from contextlib import ExitStack
+from importlib import resources
 
-lraspp_kelp_presence_onnx_path = resource_filename(
-    __name__, "LRASPP_MobileNetV3_kelp_presence_jit_miou=0.8023.pt"
+_file_manager = ExitStack()
+atexit.register(_file_manager.close)
+
+_base = resources.files(__name__)
+
+lraspp_kelp_presence_torchscript_path = _file_manager.enter_context(
+    resources.as_file(_base / "LRASPP_MobileNetV3_kelp_presence_jit_miou=0.8023.pt")
 )
-lraspp_kelp_species_onnx_path = resource_filename(
-    __name__, "LRASPP_MobileNetV3_kelp_species_jit_miou=0.9634.pt"
+lraspp_kelp_species_torchscript_path = _file_manager.enter_context(
+    resources.as_file(_base / "LRASPP_MobileNetV3_kelp_species_jit_miou=0.9634.pt")
 )
-lraspp_mussel_presence_onnx_path = resource_filename(
-    __name__, "LRASPP_MobileNetV3_mussel_presence_jit_miou=0.8384.pt"
+lraspp_mussel_presence_torchscript_path = _file_manager.enter_context(
+    resources.as_file(_base / "LRASPP_MobileNetV3_mussel_presence_jit_miou=0.8384.pt")
 )
