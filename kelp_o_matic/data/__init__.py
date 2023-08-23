237034669b0caf458bacaef74f5ac1bd2b4cb3e4
Taylor Denouden <taylordenouden@gmail.com>
2023-08-23 21:07:19 +0000

    Update mussel model with new weights trained on new dataset

--- kelp_o_matic/data/__init__.py
+++ kelp_o_matic/data/__init__.py
@@ -9,5 +9,5 @@ lraspp_kelp_species_torchscript_path = (
     _base / "LRASPP_MobileNetV3_kelp_species_jit_miou=0.9634.pt"
 )
 lraspp_mussel_presence_torchscript_path = (
-    _base / "LRASPP_MobileNetV3_mussel_presence_jit_miou=0.8362.pt"
+    _base / "LRASPP_MobileNetV3_mussel_presence_jit_miou=0.8230.pt"
 )
