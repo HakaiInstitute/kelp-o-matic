76e9792a2b3c62c511bf9ddbe06291b1c51415f5
Taylor Denouden <taylordenouden@gmail.com>
2024-01-17 14:13:57 -0800

    Fix crop size warning

--- kelp_o_matic/managers.py
+++ kelp_o_matic/managers.py
@@ -150,9 +150,9 @@ class GeotiffSegmentationManager:
             warnings.warn(
                 "Suboptimal crop_size and padding were specified. Performance "
                 "will be degraded. The detected block shape for this band is "
-                "({y_shape}, {x_shape}). Faster performance may be achieved by setting "
-                "the crop_size and the padding such that (crop_size + 2*padding) is a "
-                "multiple of {y_shape}.",
+                f"({y_shape}, {x_shape}). Faster performance may be achieved by "
+                "setting the crop_size and the padding such that "
+                f"(crop_size + 2*padding) is a multiple of {y_shape}.",
                 UserWarning,
             )
 
