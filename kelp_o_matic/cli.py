8cab1100fc7013b3015895defa9acb72b0aa4a85
Taylor Denouden <taylor.denouden@hakai.org>
2024-12-02 16:27:56 -0800

    Refactor CLI with better type hinting

--- kelp_o_matic/cli.py
+++ kelp_o_matic/cli.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Optional
+from typing import Annotated, Optional
 
 import typer
 
@@ -14,37 +14,66 @@ cli = typer.Typer(context_settings={"help_option_names": ["-h", "--help"]})
 
 @cli.command()
 def find_kelp(
-    source: Path = typer.Argument(..., help="Input image with Byte data type."),
-    dest: Path = typer.Argument(..., help="File path location to save output to."),
-    species: bool = typer.Option(
-        False,
-        "--species/--presence",
-        help="Segment to species or presence/absence level.",
-    ),
-    crop_size: int = typer.Option(
-        1024,
-        help="The data window size to run through the segmentation model.",
-    ),
-    use_nir: bool = typer.Option(
-        False,
-        "--rgbi/--rgb",
-        help="Use RGB and NIR bands for classification. Assumes RGBI ordering.",
-    ),
-    band_order: Optional[list[int]] = typer.Option(
-        None,
-        "-b",
-        help="GDAL-style band re-ordering flag. Defaults to RGB or RGBI order. "
-        "To e.g., reorder a BGRI image at runtime, pass flags `-b 3 -b 2 -b 1 -b 4`.",
-    ),
-    use_gpu: bool = typer.Option(
-        True, "--gpu/--no-gpu", help="Enable or disable GPU, if available."
-    ),
-    use_tta: bool = typer.Option(
-        False,
-        "--tta/--no-tta",
-        help="Use test time augmentation to improve accuracy at the cost of "
-        "processing time.",
-    ),
+    source: Annotated[
+        Path,
+        typer.Argument(
+            exists=True,
+            dir_okay=False,
+            file_okay=True,
+            readable=True,
+            help="Input image with Byte data type.",
+        ),
+    ],
+    dest: Annotated[
+        Path,
+        typer.Argument(
+            exists=True,
+            dir_okay=False,
+            file_okay=True,
+            writable=True,
+            help="File path location to save output to.",
+        ),
+    ],
+    species: Annotated[
+        bool,
+        typer.Option(
+            "--species/--presence",
+            help="Segment to species or presence/absence level.",
+        ),
+    ] = False,
+    crop_size: Annotated[
+        int,
+        typer.Option(
+            help="The data window size to run through the segmentation model.",
+        ),
+    ] = 1024,
+    use_nir: Annotated[
+        bool,
+        typer.Option(
+            "--rgbi/--rgb",
+            help="Use RGB and NIR bands for classification. Assumes RGBI ordering.",
+        ),
+    ] = False,
+    band_order: Annotated[
+        Optional[list[int]],
+        typer.Option(
+            "-b",
+            help="GDAL-style band re-ordering flag. Defaults to RGB or RGBI order. "
+            "To e.g., reorder a BGRI image at runtime, pass flags `-b 3 -b 2 -b 1 -b 4`.",
+        ),
+    ] = None,
+    use_gpu: Annotated[
+        bool,
+        typer.Option("--gpu/--no-gpu", help="Enable or disable GPU, if available."),
+    ] = True,
+    use_tta: Annotated[
+        bool,
+        typer.Option(
+            "--tta/--no-tta",
+            help="Use test time augmentation to improve accuracy at the cost of "
+            "processing time.",
+        ),
+    ] = False,
 ):
     """
     Detect kelp in image at path SOURCE and output the resulting classification raster
@@ -55,27 +84,52 @@ def find_kelp(
 
 @cli.command()
 def find_mussels(
-    source: Path = typer.Argument(..., help="Input image with Byte data type."),
-    dest: Path = typer.Argument(..., help="File path location to save output to."),
-    crop_size: int = typer.Option(
-        1024,
-        help="The data window size to run through the segmentation model.",
-    ),
-    band_order: Optional[list[int]] = typer.Option(
-        None,
-        "-b",
-        help="GDAL-style band re-ordering flag. Defaults to RGB order. "
-        "To e.g., reorder a BGR image at runtime, pass flags `-b 3 -b 2 -b 1`.",
-    ),
-    use_gpu: bool = typer.Option(
-        True, "--gpu/--no-gpu", help="Enable or disable GPU, if available."
-    ),
-    use_tta: bool = typer.Option(
-        False,
-        "--tta/--no-tta",
-        help="Use test time augmentation to improve accuracy at the cost of "
-        "processing time.",
-    ),
+    source: Annotated[
+        Path,
+        typer.Argument(
+            exists=True,
+            dir_okay=False,
+            file_okay=True,
+            readable=True,
+            help="Input image with Byte data type.",
+        ),
+    ],
+    dest: Annotated[
+        Path,
+        typer.Argument(
+            exists=True,
+            dir_okay=False,
+            file_okay=True,
+            writable=True,
+            help="File path location to save output to.",
+        ),
+    ],
+    crop_size: Annotated[
+        int,
+        typer.Option(
+            help="The data window size to run through the segmentation model.",
+        ),
+    ] = 1024,
+    band_order: Annotated[
+        Optional[list[int]],
+        typer.Option(
+            "-b",
+            help="GDAL-style band re-ordering flag. Defaults to RGB or RGBI order. "
+            "To e.g., reorder a BGRI image at runtime, pass flags `-b 3 -b 2 -b 1 -b 4`.",
+        ),
+    ] = None,
+    use_gpu: Annotated[
+        bool,
+        typer.Option("--gpu/--no-gpu", help="Enable or disable GPU, if available."),
+    ] = True,
+    use_tta: Annotated[
+        bool,
+        typer.Option(
+            "--tta/--no-tta",
+            help="Use test time augmentation to improve accuracy at the cost of "
+            "processing time.",
+        ),
+    ] = False,
 ):
     """
     Detect mussels in image at path SOURCE and output the resulting classification
@@ -92,9 +146,16 @@ def version_callback(value: bool) -> None:
 
 @cli.callback()
 def main(
-    version: bool = typer.Option(
-        None, "--version", "-v", callback=version_callback, is_eager=True
-    ),
+    version: Annotated[
+        bool,
+        typer.Option(
+            "--version",
+            "-v",
+            callback=version_callback,
+            is_eager=True,
+            help="Show version and exit.",
+        ),
+    ] = False,
 ):
     return
 
